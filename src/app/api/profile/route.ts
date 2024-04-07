b657f106bf375084361a251de8306e0f5491c3dd
Deniel <denielaz2020@gmail.com>
2024-04-07 17:46:46 +0200

    refactor: use auth service instead of making user validation every time

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -1,37 +1,20 @@
 import prisma from "@/lib/prisma";
-import { NextResponse } from "next/server";
-import { validate, parse } from "@tma.js/init-data-node";
+import { NextRequest, NextResponse } from "next/server";
+import { authenticateRequest } from "@/app/api/services/authenticateRequest";
 
-export async function GET(req: Request) {
-  const [authType, authData = ""] = (req.headers.get("authorization") || "").split(" ");
-  const token = process.env.MODE === "development"
-    ? process.env.DEV_BOT_TOKEN || ""
-    : process.env.BOT_TOKEN || "";
-
-  switch (authType) {
-    case "tma":
-      try {
-        const parsedAuthData = parse(authData);
-
-        validate(authData, token, {
-          expiresIn: 3600,
-        });
-        const user = await prisma.user.findUnique({
-          where: {
-            telegramId: parsedAuthData.user?.id
-          }
-        });
-
-        return NextResponse.json({ ...user, telegramId: Number(user?.telegramId) }, { status: 200 });
-      } catch (e) {
-        console.log(e);
-        return NextResponse.json({
-          message: "Error occured on authorization",
-        }, { status: 500 });
+export async function GET(req: NextRequest) {
+  try {
+    const userData = await authenticateRequest(req);
+    const user = await prisma.user.findUnique({
+      where: {
+        telegramId: userData.user?.id
       }
-    default:
-      return NextResponse.json({
-        message: "Wrong authorization type",
-      }, { status: 500 });
+    });
+  
+    return NextResponse.json({ ...user, telegramId: Number(user?.telegramId) }, { status: 200 });
+  } catch (error: Error | any) {
+    return NextResponse.json({
+      message: error.message,
+    }, { status: error.message === "Wrong authorization type" ? 400 : 500 });
   }
 }
