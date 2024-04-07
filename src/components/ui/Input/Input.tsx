3b770d129bb51014b74eeafb21c23bb24e7ace48
Deniel <denielaz2020@gmail.com>
2024-04-07 17:03:08 +0200

    refactor: Input and Textarea remove borders

--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -8,19 +8,25 @@ interface InputProps {
 	placeholder?: string;
 	invalid?: boolean;
 	label?: string;
+	min?: number;
+	max?: number;
+	inputMode?: "text" | "search" | "none" | "tel" | "url" | "email" | "numeric" | "decimal" | undefined;
 }
 
-export const Input = ({ type = "text", placeholder, value, onChange, invalid, label, checked }: InputProps) => {
+export const Input = ({ type = "text", placeholder, value, onChange, invalid, label, checked, inputMode, min, max }: InputProps) => {
 	return (
 		<div className={cls.inputWrapper}>
 			{label && <label>{label}</label>}
 			<input
 				checked={checked}
+				inputMode={inputMode}
 				value={value}
 				onChange={onChange}
 				className={cls.input + (invalid ? " " + cls.invalid : "")}
 				type={type}
 				placeholder={placeholder}
+				min={min}
+				max={max}
 			/>
 		</div>
 	);
