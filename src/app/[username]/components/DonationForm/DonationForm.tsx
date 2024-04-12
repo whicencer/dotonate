648c1a34290e4041049ad36eaed6475a209af8de
Deniel <denielaz2020@gmail.com>
2024-04-12 21:29:55 +0200

    refactor: Separate DonationForm to another component, error handling, mainButton display condition

---
+++ src/app/[username]/components/DonationForm/DonationForm.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useMainButton } from "@tma.js/sdk-react";
+import { Input } from "@/components/ui/Input/Input";
+import { Textarea } from "@/components/ui/Textarea/Textarea";
+import cls from "./styles.module.scss";
+
+interface Props {
+  minDonate: number;
+}
+
+export const DonationForm = ({ minDonate }: Props) => {
+  const [donatorName, setDonatorName] = useState("");
+  const [donationMessage, setDonationMessage] = useState("");  
+  const [tipAmount, setTipAmount] = useState(minDonate);
+  const mainButton = useMainButton();
+
+  useEffect(() => {
+    const handleClick = () => {
+      alert(`Thank you, ${donatorName} for your ${tipAmount} TON donation! Your message was: ${donationMessage}`);
+    };
+
+    mainButton.on("click", handleClick);
+    return () => mainButton.off("click", handleClick);
+  }, [tipAmount, donatorName, donationMessage, mainButton]);
+
+  return (
+    <>
+      <div>
+        <Input
+          secondary
+          label="Your nickname"
+          placeholder="John Doe"
+          value={donatorName}
+          onChange={(e) => setDonatorName(e.target.value)}
+        />
+        <Input
+          secondary
+          value={tipAmount.toString()}
+          onChange={(e) => setTipAmount(Number(e.target.value))}
+          label="Your tip amount"
+          type="number"
+          min={tipAmount}
+          inputMode="numeric"
+          placeholder="Tip"
+          style={{ marginTop: 12 }}
+        />
+        <span className={cls.minDonate}>Minimum amount: <b>{minDonate} TON</b></span>
+      </div>
+      <div style={{ marginTop: 12 }}>
+        <Textarea
+          secondary
+          placeholder="Your message here..."
+          value={donationMessage}
+          onChange={(e) => setDonationMessage(e.target.value)}
+        />
+      </div>
+    </>
+  );
+};
--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -1,68 +1,45 @@
 "use client";
 
-import { Input } from "@/components/ui/Input/Input";
-import cls from "./styles.module.scss";
-import { useEffect, useState } from "react";
-import { Textarea } from "@/components/ui/Textarea/Textarea";
+import { useEffect } from "react";
 import { useMainButton } from "@tma.js/sdk-react";
 import { useUserData } from "@/hooks/useUserData";
 import { Loader } from "@/components/ui/Loader/Loader";
+import { notFound } from "next/navigation";
+import { Panel } from "@/components/ui/Panel/Panel";
+import { DonationForm } from "./components/DonationForm/DonationForm";
 
 interface DonationPageProps {
   params: { username: string }
 }
 
 export default function DonationPage({ params }: DonationPageProps) {
-  const { user, isLoading } = useUserData(params.username);
-  const [tipAmount, setTipAmount] = useState(1);
+  const { user, isLoading, error } = useUserData(params.username);
+  
   const mainButton = useMainButton();
 
   useEffect(() => {
-    const minDonate = user?.minDonate;
-
-    if (minDonate) {
-      setTipAmount(minDonate);
+    if (!isLoading) {
+      mainButton.setText("Donate!");
+      mainButton.enable();
+      mainButton.show();
     }
-  }, [user?.minDonate]);
-
-  useEffect(() => {
-    mainButton.setText("Donate!");
-    mainButton.enable();
-    mainButton.show();
-  });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoading]);
 
   if (isLoading) return <Loader />
+  if (error) {
+    notFound();
+  }
   return (
     <div>
       <h2>Make a donation</h2>
-      <div className={cls.panel}>
-        <h3>@{user.username}</h3>
+      <Panel>
+        <h3 style={{ marginBottom: 7 }}>@{user.username}</h3>
         <span>{user.description}</span>
-      </div>
-      <div className={cls.panel}>
-        <div>
-          <Input
-            secondary
-            label="Your nickname"
-            placeholder="John Doe"
-          />
-          <Input
-            secondary
-            value={tipAmount.toString()}
-            onChange={(e) => setTipAmount(Number(e.target.value))}
-            label="Your tip amount"
-            type="number"
-            min={tipAmount}
-            inputMode="numeric"
-            placeholder="Tip"
-            style={{ marginTop: 12 }}
-          />
-          <span className={cls.minDonate}>Minimum amount: <b>{user?.minDonate} TON</b></span>
-        </div>
-        <div style={{ marginTop: 12 }}>
-          <Textarea secondary placeholder="Your message here..." />
-        </div>
-      </div>
+      </Panel>
+      <Panel>
+        <DonationForm minDonate={user.minDonate} />
+      </Panel>
     </div>
   );
 }
