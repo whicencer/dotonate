1884d6afebfe982c8afe4e76a9ade8a7000c981b
Deniel <denielaz2020@gmail.com>
2024-04-12 21:25:15 +0200

    refactor: response handling

--- src/services/getUserService.ts
+++ src/services/getUserService.ts
@@ -4,5 +4,9 @@ export async function getUserService(username: string) {
   const response = await fetch(`/api/user/${username}`);
   const user = await response.json();
 
+  if (!response.ok) {
+    throw new Error(`Error: ${response.status}`);
+  }
+
   return user as User;
 }
