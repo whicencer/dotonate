08f50ba3763c9df8a0269126dfdb85750c3c4dfd
Deniel <denielaz2020@gmail.com>
2024-04-12 21:26:01 +0200

    refactor: error handling

--- src/hooks/useUserData.ts
+++ src/hooks/useUserData.ts
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
-import { getUserProfileService } from "@/services/getUserProfileService";
 import { User } from "@/types/User";
 import { getUserService } from "@/services/getUserService";
 
 export function useUserData(username: string) {
   const [user, setUser] = useState({} as User);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     const getUserInfo = async () => {
@@ -14,15 +14,17 @@ export function useUserData(username: string) {
 
         if (user) {
           setUser(user);
-          setIsLoading(false);
         }
-      } catch (error) {
-        console.log("Error fetching user info: ", error);
+      } catch (error: Error | any) {
+        console.log(error.message);
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
       }
     };
 
     getUserInfo();
   }, [username]);
 
-  return { user, isLoading };
+  return { user, isLoading, error };
 }
