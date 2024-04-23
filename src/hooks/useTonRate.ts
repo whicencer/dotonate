ba6a2c59a651fcdcdf47c4fd4f8c669e354481d5
Deniel <denielaz2020@gmail.com>
2024-04-23 20:13:49 +0200

    feat: loading state on getting TON rate

--- src/hooks/useTonRate.ts
+++ src/hooks/useTonRate.ts
@@ -1,18 +1,25 @@
 import { useEffect, useState } from "react";
 
-export const useTonRate = (currency: string = "usd") => {
+export const useTonRate = (currency: string = "usd"): [number, boolean] => {
   const [tonRate, setTonRate] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const getTonRate = async () => {
-      const response = await fetch(`https://tonapi.io/v2/rates?tokens=ton&currencies=ton,${currency}`);
-      const data = await response.json();
-
-      setTonRate(data?.rates?.TON?.prices[currency.toUpperCase()]);
-    };
-
-    getTonRate();
+    try {
+      const getTonRate = async () => {
+        const response = await fetch(`https://tonapi.io/v2/rates?tokens=ton&currencies=ton,${currency}`);
+        const data = await response.json();
+  
+        setTonRate(data?.rates?.TON?.prices[currency.toUpperCase()]);
+      };
+  
+      getTonRate();
+    } catch (error) {
+      console.log("Error while getting ton rate: ", error);
+    } finally {
+      setIsLoading(false);
+    }
   }, [currency]);
 
-  return tonRate;
+  return [tonRate, isLoading];
 }
