d102168a4740d52fabf86f92c7e4c417fe4c2ab9
Deniel <denielaz2020@gmail.com>
2024-06-12 01:45:07 +0200

    feat: OptionSelector

--- src/components/ui/OptionSelector/OptionSelector.tsx
+++ src/components/ui/OptionSelector/OptionSelector.tsx
@@ -2,18 +2,32 @@ import { useState } from "react";
 import cls from "./styles.module.scss";
 
 interface Props {
-  options: any[];
-  // onSelect: (value: string) => void;
+  options: { label: string; value: string }[];
+  selectedOption: string;
+  onSelect?: (value: string) => void;
 }
 
-export const OptionSelector = ({ options }: Props) => {
-  const [selectedOption, setSelectedOption] = useState(null);
+export const OptionSelector = ({ options, onSelect }: Props) => {
+  const [selectedOption, setSelectedOption] = useState<string>(options[0].value);
+
+  const handleSelect = (option: string) => {
+    setSelectedOption(option);
+    if (onSelect) {
+      onSelect(option);
+    }
+  };
   
   return (
     <div className={cls.optionSelector}>
       {options.map((option, idx) => {
         return (
-          <button key={idx} className={cls.optionSelectorButton}>{option}</button>
+          <button
+            key={idx}
+            className={cls.optionSelectorButton + (selectedOption === option.value ? " " + cls.selected : "")}
+            onClick={() => handleSelect(option.value)}
+          >
+            {option.label}
+          </button>
         );
       })}
     </div>
