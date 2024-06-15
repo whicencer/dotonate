6daa6d2712c761b854ffde243ddf73cb08e911a0
Deniel <denielaz2020@gmail.com>
2024-06-15 18:56:21 +0200

    refactor: on profile edit, display popup

--- src/app/profile/edit/page.tsx
+++ src/app/profile/edit/page.tsx
@@ -11,6 +11,7 @@ import { editUser } from "./services/editUser";
 import { useNumberInput } from "@/hooks/useNumberInput";
 import { TonRate } from "@/components/TonRate/TonRate";
 import { useTonRate } from "@/hooks/useTonRate";
+import { Popup } from "@/components/ui/Popup/Popup";
 
 export default function EditProfile() {
   const initDataRaw = useInitDataRaw();
@@ -19,6 +20,8 @@ export default function EditProfile() {
 
   const [minValue, handleChangeMinValue, setMinValue] = useNumberInput("1.00");
   const [description, setDescription] = useState("");
+  const [isSuccessPopupVisible, setIsSuccessPopupVisible] = useState(false);
+  const [isErrorPopupVisible, setIsErrorPopupVisible] = useState(false);
   
   const router = useRouter();
   const backButton = useBackButton();
@@ -36,71 +39,94 @@ export default function EditProfile() {
       mainButton.show();
       mainButton.setText("Save");
       mainButton.enable();
+      backButton.show();
     }
+      
+    const handleBackButtonClick = () => {
+      router.push('/profile');
+      backButton.hide();
+      mainButton.hide();
+    };
 
-    const handlerClick = async () => {
+    const handleMainButtonClick = async () => {
       mainButton.hide();
       try {
         const response = await editUser(Number(minValue), description, initDataRaw);
         
         if (response.ok) {
-          alert("Profile has been successfully updated!");
-          router.push('/profile');
+          setIsSuccessPopupVisible(true);
           backButton.hide();
         } else {
-          alert("Failed to update profile :(");
+          setIsErrorPopupVisible(true);
           console.error("Failed to update profile");
-          mainButton.show();
         }
       } catch (error) {
         console.log("Error while updating profile: ", error);
       }
     };
 
-    mainButton.on('click', handlerClick);
+    mainButton.on('click', handleMainButtonClick);
+    backButton.on('click', handleBackButtonClick);
 
-    return () => mainButton.off('click', handlerClick);
+    return () => {
+      mainButton.off('click', handleMainButtonClick);
+      backButton.off('click', handleBackButtonClick);
+    };
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [minValue, description, initDataRaw, isLoading, router]);
 
-  useEffect(() => {
-    backButton.show();
-    backButton.on('click', () => {
-      router.push('/profile');
-      backButton.hide();
-      mainButton.hide();
-    });
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [router]);
-
   if (isLoading) return <Loader />;
   return (
-    <div>
-      <h2>Edit profile</h2>
-      <p>Change your profile settings</p>
+    <>
+      <Popup
+        title="Success"
+        message="Profile has been successfully updated!"
+        buttonText="Go to profile"
+        status="SUCCESS"
+        isOpen={isSuccessPopupVisible}
+        onClose={() => {
+          setIsSuccessPopupVisible(false);
+          router.push('/profile');
+        }}
+      />
+      <Popup
+        title="Error"
+        message="Failed to update profile :("
+        buttonText="Go to profile"
+        status="ERROR"
+        isOpen={isErrorPopupVisible}
+        onClose={() => {
+          setIsErrorPopupVisible(false);
+          router.push('/profile');
+        }}
+      />
+      <div>
+        <h2>Edit profile</h2>
+        <p>Change your profile settings</p>
 
-      <div style={{ marginTop: 20 }}>
-        <Input
-          value={minValue}
-          onChange={handleChangeMinValue}
-          type="text"
-          inputMode="decimal"
-          placeholder="Min amount"
-          label="Minimum donate amount (TON)"
-        />
-        <TonRate tonRate={tonRate * Number(minValue)} />
         <div style={{ marginTop: 20 }}>
-          <Textarea
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            placeholder="Your text"
-            label="Text on the donation page"
+          <Input
+            value={minValue}
+            onChange={handleChangeMinValue}
+            type="text"
+            inputMode="decimal"
+            placeholder="Min amount"
+            label="Minimum donate amount (TON)"
           />
+          <TonRate tonRate={tonRate * Number(minValue)} />
+          <div style={{ marginTop: 20 }}>
+            <Textarea
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              placeholder="Your text"
+              label="Text on the donation page"
+            />
+          </div>
+          <span style={{ fontSize: 12, marginTop: 12 }}>
+            Text will be displayed on the donation page
+          </span>
         </div>
-        <span style={{ fontSize: 12, marginTop: 12 }}>
-          Text will be displayed on the donation page
-        </span>
       </div>
-    </div>
+    </>
   );
 }
