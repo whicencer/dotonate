68de53823aad93712c4ab1f3054aecb5cc7166a0
Deniel <denielaz2020@gmail.com>
2024-07-19 01:21:10 +0200

    Provider for Telegram UI

--- src/components/Providers/Providers.tsx
+++ src/components/Providers/Providers.tsx
@@ -3,12 +3,15 @@
 import { ReactNode } from "react";
 import { TmaSDKLoader } from "../TmaSDKLoader";
 import { TonConnectProvider } from "./TonConnectProvider";
+import { AppRoot } from '@telegram-apps/telegram-ui';
 
 export const Providers = ({ children }: { children: ReactNode }) => {
   return (
     <TmaSDKLoader>
       <TonConnectProvider>
-        {children}
+        <AppRoot>
+          {children}
+        </AppRoot>
       </TonConnectProvider>
     </TmaSDKLoader>
   );
