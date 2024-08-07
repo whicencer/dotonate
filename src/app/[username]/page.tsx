e01f5e15560989016bd78a9b8f1417321d5980f7
Deniel <denielaz2020@gmail.com>
2024-08-07 22:20:49 +0200

    update: new panel with general information (user balance, amount to pay, ton exchange)

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -4,9 +4,11 @@ import { useUserData } from "@/hooks/useUserData";
 import { Loader } from "@/components/ui/Loader/Loader";
 import { notFound } from "next/navigation";
 import { Panel } from "@/components/ui/Panel/Panel";
-import { DonationForm } from "./components/DonationForm/DonationForm";
+import { DonationFormMemo } from "./components/DonationForm/DonationForm";
 import { TonConnectButton } from "@tonconnect/ui-react";
 import { useWallet } from "@/hooks/useWallet";
+import { useTonRate } from "@/hooks/useTonRate";
+import { useCallback, useEffect, useMemo, useState } from "react";
 
 interface DonationPageProps {
   params: { username: string }
@@ -14,26 +16,64 @@ interface DonationPageProps {
 
 export default function DonationPage({ params }: DonationPageProps) {
   const { user, isLoading, error } = useUserData(params.username);
-  const { balance } = useWallet();
+  const [donationSum, setDonationSum] = useState(0);
+  const { balance, isAuth } = useWallet();
+  const [tonRate] = useTonRate();
 
-  if (isLoading) return <Loader />
+  useEffect(() => {
+    if (user) {
+      setDonationSum(user.minDonate);
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user]);
+
+  const calculatedAmount = useMemo(() => {
+    return (donationSum * tonRate).toFixed(2);
+  }, [donationSum, tonRate]);
+
+  const onTipAmountChange = useCallback((donation: number) => {
+    setDonationSum(donation);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (isLoading) return <Loader />;
   if (error) {
     notFound();
   }
   return (
     <div>
       <h2>Make a donation</h2>
-      <div>
-        <TonConnectButton style={{ marginTop: 10 }} />
-        { balance ? <p style={{ marginTop: 10 }}>Balance: {balance} TON</p> : null }
-      </div>
+      <TonConnectButton style={{ marginTop: 10 }} />
       <Panel>
         <h3 style={{ marginBottom: 7 }}>@{user.username}</h3>
         <span>{user.description || "No description."}</span>
       </Panel>
       <Panel>
-        <DonationForm minDonate={user.minDonate} recipient={user} />
+        <DonationFormMemo
+          tonRate={tonRate}
+          minDonate={user.minDonate}
+          recipient={user}
+          transferDonationTipAmountToParent={onTipAmountChange}
+        />
       </Panel>
+      {
+        isAuth
+          ? <Panel>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <h3 style={{ marginBottom: 7, fontWeight: 400, fontSize: 16 }}>Your balance</h3>
+                <span>{balance} TON</span>
+              </div>
+              <div style={{ marginTop: 10, display: "flex", justifyContent: "space-between" }}>
+                <h3 style={{ marginBottom: 7, fontWeight: 400, fontSize: 16 }}>TON Price</h3>
+                <span>${tonRate}</span>
+              </div>
+              <div style={{ marginTop: 10, display: "flex", justifyContent: "space-between" }}>
+                <h3 style={{ marginBottom: 7, fontWeight: 400, fontSize: 16 }}>To pay</h3>
+                <span>{donationSum} TON ≈ ${calculatedAmount}</span>
+              </div>
+            </Panel>
+          : null
+      }
     </div>
   );
 }
